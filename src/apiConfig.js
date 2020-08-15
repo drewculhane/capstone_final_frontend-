67cecc584eb0402cb3e0bf28c872c460615fbc70
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-15 00:59:36 -0400

    test

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,7 +2,7 @@ let dreamwayApi;
 
 const dreamwayApis = {
   production: "https://test-dreamway-1234.herokuapp.com/",
-  // development: "http://localhost:3001/",
+  development: "http://localhost:3001/",
 };
 
 // const ideasApis = {
@@ -13,7 +13,7 @@ const dreamwayApis = {
   // if (window.location.hostname === "localhost") {
   //    dreamwayApi = dreamwayApis.development;
   //    } else {
-     dreamwayApi = dreamwayApis.production;
+     dreamwayApi = dreamwayApis.development;
 //  }
 //can't do default exports when you need to export more than one
 //changed to named exports
