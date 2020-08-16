e382bbd04aac95b0c63a1851c86aaba4c2a02c7a
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-16 13:07:49 -0400

    test

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -43,7 +43,7 @@ export default class Login extends Component {
     render() {
         return (
             <div> 
-                <form onSubmit={this.handleSubmit}>
+                <form className="lg-frm-cont" onSubmit={this.handleSubmit}>
                     <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
                     <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
                     <button type="submit" > Login </button>
