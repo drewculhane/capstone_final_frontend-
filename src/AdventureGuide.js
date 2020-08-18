a8bbc83068e2e5740644669d97739ec118e772a4
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-18 15:31:28 -0400

    test

--- src/AdventureGuide.js
+++ src/AdventureGuide.js
@@ -8,6 +8,7 @@ function AdventureGuide() {
         <DashboardLayout/> 
         <div className="Component-styles-container">
         <h1> Adventure Guide </h1>
+        <img src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1597779039/ezgif.com-video-to-gif_aihysi.gif" alt="WoW Map"/>
         </div>
         </>
     )
