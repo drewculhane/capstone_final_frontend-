2c416074e388ff823e48f9d5684489cbad1a20c6
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-18 19:24:09 -0400

    test

--- src/TalentTrees.js
+++ src/TalentTrees.js
@@ -8,8 +8,8 @@ function TalentTrees() {
         <DashboardLayout/> 
         <div className="Component-styles-container Getting-Started">
         <h1 id="comment-thread-post" className="For-feed-header"> Talent Trees: </h1>
+        <h1 id="comment-thread-post" className="For-feed-header"> | NOT MOBILE ACCESSIBLE |</h1>
         <div className="talent-cont">
-        <h1 id="comment-thread-post" className="For-feed-header"> | NOT MOBILE FRIENDLY |</h1>
         <div className="post-spef-widthcont card-choices card text-white bg-dark mb-3" >
         <div className="card-header">
             <h2> Browse all your possible talents here! Follow the link to Wowhead to start exploring possibilities. We 
