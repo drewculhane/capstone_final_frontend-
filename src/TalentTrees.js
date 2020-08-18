a53e8b0533dacf3703240972051526ebbca1fe85
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-18 19:19:28 -0400

    test

--- src/TalentTrees.js
+++ src/TalentTrees.js
@@ -8,16 +8,17 @@ function TalentTrees() {
         <DashboardLayout/> 
         <div className="Component-styles-container Getting-Started">
         <h1 id="comment-thread-post" className="For-feed-header"> Talent Trees: </h1>
-        <h1 id="comment-thread-post" className="For-feed-header"> | NOT MOBILE FRIENDLY |</h1>
         <div className="talent-cont">
-        <a className="talents" href="https://classic.wowhead.com/talent-calc/embed/druid"> Embedded. </a>
-        </div>
+        <h1 id="comment-thread-post" className="For-feed-header"> | NOT MOBILE FRIENDLY |</h1>
         <div className="post-spef-widthcont card-choices card text-white bg-dark mb-3" >
         <div className="card-header">
             <h2> Browse all your possible talents here! Follow the link to Wowhead to start exploring possibilities. We 
                 reccommend using a tablet or laptop. </h2>
         </div>
         </div>
+        <a className="talents" href="https://classic.wowhead.com/talent-calc/embed/druid"> Embedded. </a>
+        </div>
+        
         </div> 
         </>
     )
