e4b6b28774a3f453cdedb431d724dc967c75e542
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-18 18:04:31 -0400

    test

--- src/TalentTrees.js
+++ src/TalentTrees.js
@@ -8,12 +8,8 @@ function TalentTrees() {
         <DashboardLayout/> 
         <div className="Component-styles-container">
         <h1> Talent Trees </h1>
-        <div className="post-spef-widthcont card-choices card text-white bg-dark mb-3" >
-        <div className="card-header">
         <a href="https://classic.wowhead.com/talent-calc/embed/druid/014005001-5050501303022151-05002"> Embedded. </a>
         </div>
-        </div>
-        </div> 
         </>
     )
 }
