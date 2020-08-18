7eb2781bdf7772e06fa007783a4417e6949a5994
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-18 15:45:59 -0400

    test

--- src/AdventureGuide.js
+++ src/AdventureGuide.js
@@ -6,11 +6,11 @@ function AdventureGuide() {
     return (
         <>
         <DashboardLayout/> 
-        <div id="g-s" className="Component-styles-container Getting-Started">
+        <div id="g-s" className="Component-styles-container Getting-Started ">
         <h1 id="comment-thread-post" className="For-feed-header"> Adventure Guide: </h1>
-        <div className="post-spef-widthcont card-choices card text-white bg-dark mb-3" >
+        <div className="post-spef-widthcont card-choices card text-white bg-dark mb-3 adv-guide" >
         <div className="card-header">
-        <img src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1597779039/ezgif.com-video-to-gif_aihysi.gif" alt="WoW Map"/>
+        <img className="map-gif" src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1597779039/ezgif.com-video-to-gif_aihysi.gif" alt="WoW Map"/>
         </div> 
         </div> 
         </div> 
