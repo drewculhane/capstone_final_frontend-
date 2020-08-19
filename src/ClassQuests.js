f1161884c63a89f64106ce81431c7fb4c95d1cf8
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-19 00:19:42 -0400

    test

--- src/ClassQuests.js
+++ src/ClassQuests.js
@@ -11,28 +11,19 @@ function ClassQuests() {
         <div className="post-spef-widthcont card-choices card text-white bg-dark mb-3" >
         <div className="card-header">
             <ul> 
-                <li> BiS Resto Druid Gear:  <a href="https://www.icy-veins.com/wow-classic/restoration-druid-healer-pve-gear-best-in-slot" target="_blank"> CLICK HERE <span classname="greenwrap"> </span></a></li>
-                <li>  BiS Balance Druid Gear:<a href="https://www.icy-veins.com/wow-classic/balance-druid-ranged-dps-pve-gear-best-in-slot" target="_blank"> <span classname="greenwrap"> CLICK HERE </span></a></li>
-                <li>  BiS Feral/Tank Druid Gear: <a href="https://www.icy-veins.com/wow-classic/feral-druid-tank-pve-gear-best-in-slot" target="_blank"> <span classname="greenwrap"> CLICK HERE </span></a></li>
-                <li> Druid Class Quests:  <a href="https://classic.wowhead.com/guides/druid-class-quests-classic-wow" target="_blank"> <span classname="greenwrap"> CLICK HERE </span></a></li>
+                <li> BiS Resto Druid Gear:  <a href="https://www.icy-veins.com/wow-classic/restoration-druid-healer-pve-gear-best-in-slot" target="_blank"> <span className="greenwrap">CLICK HERE  </span></a></li>
+                <li>  BiS Balance Druid Gear:<a href="https://www.icy-veins.com/wow-classic/balance-druid-ranged-dps-pve-gear-best-in-slot" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
+                <li>  BiS Feral/Tank Druid Gear: <a href="https://www.icy-veins.com/wow-classic/feral-druid-tank-pve-gear-best-in-slot" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
+                <li> Druid Class Quests:  <a href="https://classic.wowhead.com/guides/druid-class-quests-classic-wow" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
             </ul>
         </div>
         </div> 
         <div className="post-spef-widthcont card-choices card text-white bg-dark mb-3" >
         <div className="card-header">
             <ul> 
-            <li> Druid AddOns Galore! :  <a href="https://www.icy-veins.com/wow-classic/classic-guide-to-druids-best-addons" target="_blank"> <span classname="greenwrap"> CLICK HERE </span></a></li>
-            <li> Quest support and more on WoWhead Classic! In case Questie bugs out. <a href="" target="_blank"> <span classname="greenwrap"> CLICK HERE </span></a></li>
-            </ul>
-        </div>
-        </div> 
-        <div className="post-spef-widthcont card-choices card text-white bg-dark mb-3" >
-        <div className="card-header">
-            <ul> 
-                <li> <span classname="greenwrap">   </span></li>
-                <li> <span classname="greenwrap">   </span></li>
-                <li> <span classname="greenwrap">   </span></li>
-                <li> <span classname="greenwrap">   </span></li>
+            <li> Druid AddOns Galore! :  <a href="https://www.icy-veins.com/wow-classic/classic-guide-to-druids-best-addons" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
+            <li> Sometimes Questie bugs out: </li>
+            <li> Quest support and more on WoWhead Classic!  <a href="" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
             </ul>
         </div>
         </div> 
