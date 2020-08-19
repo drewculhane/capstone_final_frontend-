d56771ffe297f1f725024748d79bcde67dc9be58
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-19 00:41:17 -0400

    test

--- src/ClassQuests.js
+++ src/ClassQuests.js
@@ -14,7 +14,7 @@ function ClassQuests() {
                 <li> BiS Resto Druid Gear:  <a href="https://www.icy-veins.com/wow-classic/restoration-druid-healer-pve-gear-best-in-slot" target="_blank"> <span className="greenwrap">CLICK HERE  </span></a></li>
                 <li>  BiS Balance Druid Gear:<a href="https://www.icy-veins.com/wow-classic/balance-druid-ranged-dps-pve-gear-best-in-slot" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
                 <li>  BiS Feral/Tank Druid Gear: <a href="https://www.icy-veins.com/wow-classic/feral-druid-tank-pve-gear-best-in-slot" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
-                <li> Druid Class Quests:  <a href="https://classic.wowhead.com/guides/druid-class-quests-classic-wow" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
+                <li> Druid Class Quests: <span className="greenwrap"> <a href="https://classic.wowhead.com/guides/druid-class-quests-classic-wow" target="_blank">  </span></a></li>
             </ul>
         </div>
         </div> 
@@ -22,8 +22,8 @@ function ClassQuests() {
         <div className="card-header">
             <ul> 
             <li> Druid AddOns Galore! :  <a href="https://www.icy-veins.com/wow-classic/classic-guide-to-druids-best-addons" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
-            <li> Sometimes Questie bugs out: </li>
-            <li> Quest support and more on WoWhead Classic!  <a href="" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
+            <li> Sometimes Questie bugs out.... </li>
+            <li> ...lucky there is Quest support and more on WoWhead Classic!  <a href="https://classic.wowhead.com/" target="_blank"> <span className="greenwrap"> CLICK HERE </span></a></li>
             </ul>
         </div>
         </div> 
